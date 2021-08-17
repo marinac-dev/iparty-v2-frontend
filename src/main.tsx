fddc824bcd7b1ad5de51061b8c0de6003d1a268a
Marinac <nikola@regex-sh.com>
2021-08-17 14:53:04 +0200

    Remove boilerplate

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+// Imports
 import "./main.css";
-import App from "./App";
+import App from "./components/App";
 
 ReactDOM.render(
   <React.StrictMode>
