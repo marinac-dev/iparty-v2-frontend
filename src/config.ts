3e982de07dfd95ddedca488973003c9ce4f728d3
Marinac <nikola@regex-sh.com>
2021-08-27 23:16:06 +0200

    Add global config file

---
+++ src/config.ts
@@ -0,0 +1,22 @@
+const Config: any = {};
+
+Config.PlayerOpts = {
+  videoId: "ele2DMU49Jk",
+  opts: {
+    max_height: "390",
+    width: "640",
+    playerVars: {
+      fs: 0,
+      rel: 0,
+      controls: 0,
+      disablekb: 1,
+      enablejsapi: 1,
+      playsinline: 1,
+      modestbranding: 1,
+      cc_load_policy: 0,
+      origin: window.location.hostname,
+    },
+  },
+};
+
+export default Config;
