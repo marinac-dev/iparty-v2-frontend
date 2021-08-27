846ccfa0805708afe1add482c54dd8b729feeea7
Marinac <nikola@regex-sh.com>
2021-08-27 23:16:57 +0200

    Update iParty page

--- src/components/player/IPartyPlayer.tsx
+++ src/components/player/IPartyPlayer.tsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+import { Player } from "./Player";
 
 interface Props {}
 
 export const IPartyPlayer: React.FC<Props> = () => {
   return (
-    <div className="flex flex-col justify-start items-center mx-4 my-2 rounded py-4">
-      {/* Player | Search */}
-      <div className="flex flex-col lg:flex-row bg-gray-300 dark:bg-erie-black-800 rounded-md p-2 w-full justify-center items-center">
-        {/* Recommend & History | Player  */}
-        <div className="flex flex-col lg:flex-row justify-center items-center w-full lg:w-2/3"></div>
+    <div className="iparty">
+      {/* Player & Search */}
+      <div className="player-and-search ios-bg-clear">
+        <Player />
+        {/* <Search /> */}
       </div>
       {/* PlayList */}
     </div>
