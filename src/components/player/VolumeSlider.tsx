e18fa9580cc421af707c98c9edd83170b504ed96
Marinac <nikola@regex-sh.com>
2021-08-28 23:47:16 +0200

    Add volume slider

---
+++ src/components/player/VolumeSlider.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+interface Props {
+  soundLevel: number;
+  handleCallback: any;
+}
+
+export const VolumeSlider: React.FC<Props> = ({ soundLevel, handleCallback }) => {
+  return (
+    <input
+      type="range"
+      min={0}
+      step={1}
+      max={100}
+      value={soundLevel}
+      className="range-bar"
+      onInput={handleCallback}
+      defaultValue={soundLevel}
+    />
+  );
+};
