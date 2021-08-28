bd37daf502ad966ae808801e2dafdee0e65e96d5
Marinac <nikola@regex-sh.com>
2021-08-28 19:02:40 +0200

    Expose player to controls

--- src/components/player/Player.tsx
+++ src/components/player/Player.tsx
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 import YouTube from "react-youtube";
-import Config from "../../config";
+import Config from "../../Config";
 import { Controls } from "./Controls";
 
 interface Props {}
 export const Player: React.FC<Props> = () => {
+  const [player, setPlayer] = useState<HTMLInputElement>();
+
+  function _onReady(event: { target: HTMLInputElement }) {
+    setPlayer(event.target);
+  }
+
   return (
     <div className="wrapper">
       <div className="player" id="player_awning" data-content="">
         {/* <!-- Video player --> */}
-        <YouTube className="w-full pointer-events-none" videoId={Config.PlayerOpts.videoId} opts={Config.PlayerOpts.opts}/>
+        <YouTube containerClassName="video" onReady={_onReady} videoId={Config.PlayerOpts.videoId} opts={Config.PlayerOpts.opts}/>
         {/* <!-- Controls --> */}
-        <Controls />   
+        <Controls player={player} />
       </div>
     </div>
   );
