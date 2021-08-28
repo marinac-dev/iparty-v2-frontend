9f058bb81041156baf80a43fdd94a11bf97afb80
Marinac <nikola@regex-sh.com>
2021-08-29 00:09:31 +0200

    Fix volume slider

--- src/components/player/VolumeSlider.tsx
+++ src/components/player/VolumeSlider.tsx
@@ -7,15 +7,6 @@ interface Props {
 
 export const VolumeSlider: React.FC<Props> = ({ soundLevel, handleCallback }) => {
   return (
-    <input
-      type="range"
-      min={0}
-      step={1}
-      max={100}
-      value={soundLevel}
-      className="range-bar"
-      onInput={handleCallback}
-      defaultValue={soundLevel}
-    />
+    <input type="range" min={0} step={1} max={100} value={soundLevel} className="range-bar" onInput={handleCallback} />
   );
 };
