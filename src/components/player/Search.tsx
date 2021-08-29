3cba936805b6518cd724650a96f65fb17a965e20
Marinac <nikola@regex-sh.com>
2021-08-29 12:16:47 +0200

    Update Search

--- src/components/player/Search.tsx
+++ src/components/player/Search.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 interface Props {}
+import { SearchBar } from "./SearchBar";
+import { SearchResults } from "./SearchResults";
 
 export const Search: React.FC<Props> = () => {
-  return <div className="search">
-    <input type="search" name="" id="" />
-    https://www.youtube.com/watch?v=ele2DMU49Jk
-  </div>;
+  return (
+    <div className="search">
+      <SearchBar />
+      <SearchResults />
+    </div>
+  );
 };
